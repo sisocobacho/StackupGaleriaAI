4acf5a3b1b125e4df270f9273135e441e7b895d2
sisocobacho <110941609+sisocobacho@users.noreply.github.com>
2025-05-13 14:23:00 +0000

    using env for react

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+   baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 // Add auth token to requests
